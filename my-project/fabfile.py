e280579eba28f7c32d803374f524090aa9a56f49
Carlos <carlsjdh@gmail.com>
2018-01-29 13:27:03 +0000

    project

--- my-project/fabfile.py
+++ my-project/fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import env, cd, local, run
+from fabric.api import env, cd, local, run, prefix
 
 # nombre de la máquina de producción
 env.hosts = ["cloud"]
@@ -8,6 +8,7 @@ env.hosts = ["cloud"]
 
 def deploy():
     local("git push")
-    with cd("~/mkdocs"):
-        run("git pull")
-        run("cd my-project && source /home/alu5896/.virtualenvs/mkdocs/bin/activate && mkdocs build")
+    with prefix("source /home/alu5896/.virtualenvs/mkdocs/bin/activate"):
+        with cd("~/mkdocs"):
+            run("git pull")
+            run("cd my-project && mkdocs build")
